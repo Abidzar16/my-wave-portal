35c41cc6d25e341ae100c6c6006b964433cc11d3
Abidzar M. G. Kurniawan <abidzarisprrivate@gmail.com>
2022-07-16 20:48:45 +0700

    Store 👋 data on our smart contract

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,7 +5,19 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    uint256 totalWaves;
+
     constructor() {
-        console.log("Yo yo, I am a contract and I am smart");
+        console.log("Yo yo, I am a contract that is smart");
+    }
+
+    function wave() public {
+        totalWaves += 1;
+        console.log("%s has hands waved!", msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total waves together!", totalWaves);
+        return totalWaves;
     }
 }
